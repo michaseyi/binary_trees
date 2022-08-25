0e59b94239ce491322ea235040766871547ce6ed
Adewole Michael <michaseyi@gmail.com>
2022-08-26 00:26:43 +0100

    Added task 26

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -14,9 +14,6 @@ bst_t *array_to_bst(int *array, size_t size)
 	bst_t *root = NULL;
 
 	for (i = 0; i < size; i++)
-	{
-		if (!bst_insert(&root, array[i]))
-			return (NULL);
-	}
+		bst_insert(&root, array[i]);
 	return (root);
 }
