e76b99cb109d8ff7aa0a5fe30a78fc3bbf5e0fc4
Adewole Michael <michaseyi@gmail.com>
2022-08-25 13:45:30 +0100

    Added task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,6 +2,21 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 /**
+ * _binary_tree_height - measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height
+ * Return: is the height of the tree or 0 if the tree param is NULL
+ */
+
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	return (1 + MAX(binary_tree_height(tree->left),
+						 binary_tree_height(tree->right)));
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: is a pointer to the root node of the tree to measure the height
  * Return: is the height of the tree or 0 if the tree param is NULL
@@ -10,8 +25,6 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (-1);
-
-	return (1 + MAX(binary_tree_height(tree->left),
-						 binary_tree_height(tree->right)));
+		return (0);
+	return (_binary_tree_height(tree));
 }
