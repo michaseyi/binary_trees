b34cfeea9059b6d0a32e9ce9ce95df766f78456d
Adewole Michael <michaseyi@gmail.com>
2022-08-25 13:33:20 +0100

    Added task 6, 7, 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,11 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - goes through a binary tree using inorder traversal
+ * @tree: is a pointer to the root node of the three to traverse
+ * @func: is a poitner to a function to call for each node
+ */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-   if (tree == NULL)
-      return;
+	if (tree == NULL)
+		return;
 
-   binary_tree_inorder(tree->left, func);
-   func(tree->n);
-   binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,16 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - goes through a binary tree using postorder traversal
+ * @tree: is a pointer to the root node of the three to traverse
+ * @func: is a poitner to a function to call for each node
+ */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-   if (tree == NULL)
-      return;
-
-   func(tree->n);
-   binary_tree_postorder(tree->left, func);
-   binary_tree_postorder(tree->right, func);
+	if (tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 }
